2d7c0f969612d02d29c4fdd9feadfa31ca7375f2
Tyler Timm <ttimm16@gmail.com>
2024-12-02 22:59:05 -0600

    Add python and Airflow skills

--- src/components/SkillsList.js
+++ src/components/SkillsList.js
@@ -1,52 +1,51 @@
 import styles from '@/styles/Home.module.css';
 
-import { FaJs, FaHtml5, FaCss3, FaFilePdf, FaMobile, FaAws } from "react-icons/fa";
-import { SiMicrosoftoffice, SiOracle } from 'react-icons/si';
+import { FaJs, FaHtml5, FaCss3, FaFilePdf, FaMobile, FaAws, FaPython } from "react-icons/fa";
+import { SiMicrosoftoffice, SiOracle, SiApacheairflow } from 'react-icons/si';
 import { MdAdminPanelSettings } from "react-icons/md";
 
 const skills = [
     {
-        key: 0,
         name: 'JavaScript',
         icon: <FaJs />
     },
     {
-        key: 1,
+        name: 'Python',
+        icon: <FaPython />
+    },
+    {
         name: 'NetSuite Customization and Integration',
         icon: <SiOracle />
     },
     {
-        key: 2,
         name: 'AWS - Lambda, DynamoDB, Amplify, Appsync',
         icon: <FaAws />
     },
     {
-        key: 3,
+        name: 'Airflow',
+        icon: <SiApacheairflow />
+    },
+    {
         name: 'HTML',
         icon: <FaHtml5 />
     },
     {
-        key: 4,
         name: 'CSS',
         icon: <FaCss3 />
     },
     {
-        key: 5,
         name: 'FreeMarker',
         icon: <FaFilePdf />
     },
     {
-        key: 6,
         name: 'NetSuite Administration',
         icon: <MdAdminPanelSettings />
     },
     {
-        key: 7,
         name: 'Microsoft 365 Administration',
         icon: <SiMicrosoftoffice />
     },
     {
-        key: 8,
         name: 'Mobile Device Management',
         icon: <FaMobile />
     }
